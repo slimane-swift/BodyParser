a144dcc508366291110cc371f7345ff20cd82dcd
noppoman <yuki@miketokyo.com>
2016-05-11 02:57:44 +0900

    update dependencies

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 let package = Package(
 	name: "BodyParser",
 	dependencies: [
-      .Package(url: "https://github.com/slimane-swift/Middleware.git", majorVersion: 0, minor: 2),
-      .Package(url: "https://github.com/slimane-swift/JSON.git", majorVersion: 0, minor: 6)
+      .Package(url: "https://github.com/slimane-swift/Middleware.git", majorVersion: 0, minor: 3),
+      .Package(url: "https://github.com/slimane-swift/JSON.git", majorVersion: 0, minor: 7)
   ]
 )
