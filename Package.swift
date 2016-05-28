e17a3915e162cce634982a08a97595a212e6b219
noppoman <yuki@miketokyo.com>
2016-05-29 07:16:26 +0900

    Bump Middleware to 0.4

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 let package = Package(
 	name: "BodyParser",
 	dependencies: [
-      .Package(url: "https://github.com/slimane-swift/Middleware.git", majorVersion: 0, minor: 3),
+      .Package(url: "https://github.com/slimane-swift/Middleware.git", majorVersion: 0, minor: 4),
       .Package(url: "https://github.com/slimane-swift/JSON.git", majorVersion: 0, minor: 7)
   ]
 )
